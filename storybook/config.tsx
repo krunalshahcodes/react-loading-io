08f5a07cf8cbedba1dabef4f13ba3129b4dc4c3d
Krunal Shah <shahkrunal7091@gmail.com>
2019-08-28 12:39:42 +0530

    fix: fix previous commit

---
+++ storybook/config.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import styled from '@emotion/styled'
+import {
+  addParameters,
+  RenderFunction,
+  addDecorator,
+  configure
+} from '@storybook/react'
+import { withKnobs } from '@storybook/addon-knobs'
+
+addParameters({
+  options: {
+    brandUrl: 'https://imkrunal.github.io/react-loading-io/'
+  }
+})
+
+const Container = styled('div')`
+  background-color: #20232a;
+  padding: 1rem 0.5rem;
+  display: flex;
+  align-items: center;
+  > div {
+    margin: 0 1rem;
+  }
+`
+
+const Decorator = (storyFn: RenderFunction): JSX.Element => (
+  <Container>{storyFn()}</Container>
+)
+
+addDecorator(Decorator)
+
+addDecorator(withKnobs)
+
+const req = require.context('../src', true, /\.story\.tsx$/)
+
+const loadStories = (): void => {
+  req.keys().forEach(req)
+}
+
+configure(loadStories, module)
